e01473f25f8703fe2456c0d7cd0abdb80def03a3
Beverly Thompson <blt1339@gmail.com>
2020-12-23 14:32:01 -0500

    Add logic to output the demographics information for chosen test ID.

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,7 @@
+// Process when the test ID changes
+function optionChanged(id) {
+    getDemoData(id);
+}
 
 
 function init() {
@@ -9,6 +13,9 @@ function init() {
         for (let i = 0; i < data.names.length; i++) {
             dropDown.append("option").text("BB_" + data.names[i]).property("value");
         }
+     
+        // Update demographics for the first ID 
+        getDemoData("BB_" + data.names[0]);
 
         // data.names.forEach(function(name) {
         //     dropDown.append("option").text(name).property("value");
@@ -18,6 +25,34 @@ function init() {
     })
 }
 
+// Output the demographics information for chosen test id
+function getDemoData(id) {
+    let idParts = id.split('_')
+    let idNo = idParts[1]
+    console.log(idParts);
+    console.log(idNo);
+    
 
+
+    // Use D3 fetch to read the JSON file
+    d3.json("data/samples.json").then((data) => {
+
+        let metadata = data.metadata;
+        console.log(metadata);
+        var sampleMetadata = metadata.filter(metaInfo => metaInfo.id.toString() === idNo)[0];
+        console.log(sampleMetadata);
+
+        let panelBody = d3.select('#sample-metadata')
+        console.log(panelBody);
+
+        panelBody.html("");
+
+        Object.entries(sampleMetadata).forEach((key) => {   
+                panelBody.append("h5").text(key[0] + ": " + key[1] + "\n");    
+        });
+
+
+    })
+}
 
 init();
