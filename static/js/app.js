854b543dbec6158e4d239990149c9aac9375a7c5
Beverly Thompson <blt1339@gmail.com>
2020-12-23 13:57:40 -0500

    Create init to fill the drop down with the ids.

--- static/js/app.js
+++ static/js/app.js
@@ -1,45 +1,23 @@
-// Use D3 fetch to read the JSON file
-// The data from the JSON file is arbitrarily named importedData as the argument
-d3.json("data/samples.json").then((importedData) => {
-    // console.log(importedData);
-    var samples = importedData;
-    console.log(samples);
-  
-    // // Sort the data array using the greekSearchResults value
-    // data.sort(function(a, b) {
-    //   return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-    // });
-  
-    // // Slice the first 10 objects for plotting
-    // data = data.slice(0, 10);
-  
-    // // Reverse the array due to Plotly's defaults
-    // data = data.reverse();
-  
-    // // Trace1 for the Greek Data
-    // var trace1 = {
-    //   x: data.map(row => row.greekSearchResults),
-    //   y: data.map(row => row.greekName),
-    //   text: data.map(row => row.greekName),
-    //   name: "Greek",
-    //   type: "bar",
-    //   orientation: "h"
-    // };
-  
-    // // data
-    // var chartData = [trace1];
-  
-    // // Apply the group bar mode to the layout
-    // var layout = {
-    //   title: "Greek gods search results",
-    //   margin: {
-    //     l: 100,
-    //     r: 100,
-    //     t: 100,
-    //     b: 100
-    //   }
-    // };
-  
-    // // Render the plot to the div tag with id "plot"
-    // Plotly.newPlot("plot", chartData, layout);
-  });
+
+
+function init() {
+    let dropDown = d3.select("#selDataset");
+
+    // Use D3 fetch to read the JSON file
+    d3.json("data/samples.json").then((data) => {
+
+        for (let i = 0; i < data.names.length; i++) {
+            dropDown.append("option").text("BB_" + data.names[i]).property("value");
+        }
+
+        // data.names.forEach(function(name) {
+        //     dropDown.append("option").text(name).property("value");
+        // });
+
+        // data.names.forEach(name => dropDown.append("option").text(name).property("value"));
+    })
+}
+
+
+
+init();
